56b4c5e1c47cf96caee42168ec92c484255d54a7
Matthew Witkowski <mwitkowski@figure.com>
2023-01-30 15:38:58 -0500

    Fixed issue in withdraw_capital signature.

--- src/execute/withdraw_capital.rs
+++ src/execute/withdraw_capital.rs
@@ -2,6 +2,6 @@ use cosmwasm_std::{Addr, Response};
 
 use crate::core::aliases::ProvTxResponse;
 
-pub fn withdraw_capital(sender: &Addr) -> ProvTxResponse {
+pub fn withdraw_capital(_sender: Addr) -> ProvTxResponse {
     Ok(Response::default())
 }
