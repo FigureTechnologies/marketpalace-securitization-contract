105ae67505625505bf6cead2fa48755c9e74e1c2
Matthew Witkowski <mwitkowski@figure.com>
2023-02-08 17:05:06 -0500

    Added tests for instantiate.

--- src/instantiate/mod.rs
+++ src/instantiate/mod.rs
@@ -1,8 +1,8 @@
 use cosmwasm_std::{Addr, CosmosMsg, Env, MessageInfo, Response, StdResult};
 use cw2::set_contract_version;
 use provwasm_std::{
-    activate_marker, create_marker, finalize_marker, grant_marker_access, withdraw_coins,
-    MarkerAccess, MarkerType, ProvenanceMsg,
+    activate_marker, create_marker, finalize_marker, grant_marker_access, MarkerAccess, MarkerType,
+    ProvenanceMsg,
 };
 
 pub mod validate;
@@ -20,11 +20,11 @@ use crate::{
 pub fn handle(
     deps: ProvDepsMut,
     env: Env,
-    info: MessageInfo,
+    _info: MessageInfo,
     msg: InstantiateMsg,
 ) -> ProvTxResponse {
     set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
-    let state = State::new(info.sender, msg.capital_denom, msg.rules);
+    let state = State::new(msg.gp, msg.capital_denom, msg.rules);
     STATE.save(deps.storage, &state)?;
 
     // Create the markers
@@ -38,7 +38,9 @@ pub fn handle(
         SECURITIES_MAP.save(deps.storage, security.name.clone(), security)?;
     }
 
-    Ok(Response::default().add_messages(messages))
+    Ok(Response::default()
+        .add_messages(messages)
+        .add_attribute("action", "init"))
 }
 
 fn new_active_marker(
@@ -57,29 +59,157 @@ fn new_active_marker(
         grant_marker_access(denom, owner.clone(), permissions)?,
         finalize_marker(denom)?,
         activate_marker(denom)?,
-        withdraw_coins(denom, amount, denom, owner)?,
     ])
 }
 
 #[cfg(test)]
 mod tests {
+    use cosmwasm_std::Attribute;
+    use cosmwasm_std::{
+        testing::{mock_env, mock_info},
+        Addr, Coin, StdError,
+    };
+    use cw2::get_contract_version;
+    use provwasm_mocks::mock_dependencies;
+    use provwasm_std::{
+        activate_marker, create_marker, finalize_marker, grant_marker_access, MarkerAccess,
+        MarkerType,
+    };
+
+    use crate::{
+        contract::instantiate,
+        core::{
+            constants::{CONTRACT_NAME, CONTRACT_VERSION},
+            msg::InstantiateMsg,
+            rules::InvestmentVehicleRule,
+            security::{Security, TrancheSecurity},
+            state::{SECURITIES_MAP, STATE},
+        },
+        instantiate::new_active_marker,
+    };
+
     #[test]
     fn test_new_active_marker_creates_and_activates_marker() {
-        assert!(false);
+        let address = Addr::unchecked("address");
+        let denom = "denom".to_string();
+        let amount = 1000;
+        let permissions = vec![
+            MarkerAccess::Admin,
+            MarkerAccess::Mint,
+            MarkerAccess::Burn,
+            MarkerAccess::Withdraw,
+        ];
+
+        let messages = new_active_marker(address.clone(), &denom, amount).unwrap();
+        assert_eq!(4, messages.len());
+        assert_eq!(
+            create_marker(amount, &denom, MarkerType::Coin).unwrap(),
+            messages[0]
+        );
+        assert_eq!(
+            grant_marker_access(&denom, address.clone(), permissions,).unwrap(),
+            messages[1]
+        );
+        assert_eq!(finalize_marker(&denom).unwrap(), messages[2]);
+        assert_eq!(activate_marker(&denom).unwrap(), messages[3]);
     }
 
     #[test]
     fn test_new_active_marker_throws_errors_on_invalid_marker_txs() {
-        assert!(false);
+        let bad_addr =
+            new_active_marker(Addr::unchecked(""), &"mycustomdenom".to_string(), 1000).unwrap_err();
+        let expected = StdError::generic_err("address must not be empty");
+        assert_eq!(expected, bad_addr);
+
+        let bad_denom =
+            new_active_marker(Addr::unchecked("address"), &"".to_string(), 1000).unwrap_err();
+        let expected = StdError::generic_err("denom must not be empty");
+        assert_eq!(expected, bad_denom);
     }
 
     #[test]
-    fn test_handle_sets_the_contract_version() {
-        assert!(false);
+    fn test_with_valid_data() {
+        // create valid init data
+        let mut deps = mock_dependencies(&[]);
+        let info = mock_info("admin", &[]);
+        const DEFAULT_GP: &str = "gp";
+        const DEFAULT_RULES: Vec<InvestmentVehicleRule> = vec![];
+        const DEFAULT_CAPITAL_DENOM: &str = "denom";
+        let securities = vec![
+            Security {
+                name: "Tranche 1".to_string(),
+                amount: 1000,
+                minimum_amount: 100,
+                price_per_unit: Coin::new(100, "denom"),
+                security_type: crate::core::security::SecurityType::Tranche(TrancheSecurity {}),
+            },
+            Security {
+                name: "Tranche 2".to_string(),
+                amount: 1000,
+                minimum_amount: 100,
+                price_per_unit: Coin::new(100, "denom"),
+                security_type: crate::core::security::SecurityType::Tranche(TrancheSecurity {}),
+            },
+        ];
+        let init_msg = InstantiateMsg {
+            gp: Addr::unchecked(DEFAULT_GP),
+            securities: securities.clone(),
+            capital_denom: DEFAULT_CAPITAL_DENOM.to_string(),
+            rules: DEFAULT_RULES,
+        };
+
+        // initialize
+        let init_response = instantiate(deps.as_mut(), mock_env(), info, init_msg.clone());
+
+        // Check the messages
+        match init_response {
+            Ok(res) => {
+                assert_eq!(8, res.messages.len());
+                // We probably want to check the type of messages
+                assert_eq!(1, res.attributes.len());
+                assert_eq!(Attribute::new("action", "init"), res.attributes[0])
+            }
+            Err(error) => panic!("unable to initialize contract {}", error),
+        };
+
+        // Check the contract version
+        let contract_version = get_contract_version(&deps.storage).unwrap();
+        assert_eq!(CONTRACT_VERSION, contract_version.version);
+        assert_eq!(CONTRACT_NAME, contract_version.contract);
+
+        // Check the STATE
+        let state = STATE.load(&deps.storage).unwrap();
+        assert_eq!(DEFAULT_CAPITAL_DENOM.to_string(), state.capital_denom);
+        assert_eq!(Addr::unchecked(DEFAULT_GP), state.gp);
+        assert_eq!(DEFAULT_RULES, state.rules);
+
+        // Check the SECURITIES_MAP
+        for security in securities {
+            let saved = SECURITIES_MAP
+                .load(&deps.storage, security.name.clone())
+                .unwrap();
+            assert_eq!(security, saved);
+        }
     }
 
     #[test]
-    fn test_success() {
-        assert!(false);
+    fn test_with_invalid_data() {
+        // create valid init data
+        let mut deps = mock_dependencies(&[]);
+        let info = mock_info("admin", &[]);
+        const DEFAULT_GP: &str = "gp";
+        const DEFAULT_RULES: Vec<InvestmentVehicleRule> = vec![];
+        const DEFAULT_CAPITAL_DENOM: &str = "denom";
+        let securities = vec![];
+        let init_msg = InstantiateMsg {
+            gp: Addr::unchecked(DEFAULT_GP),
+            securities: securities.clone(),
+            capital_denom: DEFAULT_CAPITAL_DENOM.to_string(),
+            rules: DEFAULT_RULES,
+        };
+
+        // initialize
+        let res = instantiate(deps.as_mut(), mock_env(), info, init_msg.clone());
+        res.expect_err("expected error in invalid instantiate");
     }
 }
