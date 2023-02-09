f4795bcb4745f9b62febeab2fe4b72accc1b1122
Matthew Witkowski <mwitkowski@figure.com>
2023-02-09 09:18:43 -0500

    Added tests for commitment.

--- src/execute/settlement/commitment.rs
+++ src/execute/settlement/commitment.rs
@@ -32,3 +32,44 @@ pub enum CommitmentState {
     ACCEPTED,
     SETTLED,
 }
+
+#[cfg(test)]
+mod tests {
+    use cosmwasm_std::Addr;
+
+    use crate::{
+        core::security::SecurityCommitment, execute::settlement::commitment::CommitmentState,
+    };
+
+    use super::Commitment;
+
+    #[test]
+    fn test_clear_amounts() {
+        let lp = Addr::unchecked("address");
+        let securities = vec![SecurityCommitment {
+            name: "security 1".to_string(),
+            amount: 5,
+        }];
+        let mut commitment = Commitment::new(lp.clone(), securities.clone());
+
+        commitment.clear_amounts();
+        assert_eq!(securities.len(), commitment.commitments.len());
+        for security in &commitment.commitments {
+            assert_eq!(0, security.amount);
+        }
+    }
+
+    #[test]
+    fn test_new() {
+        let lp = Addr::unchecked("address");
+        let securities = vec![SecurityCommitment {
+            name: "security 1".to_string(),
+            amount: 5,
+        }];
+        let commitment = Commitment::new(lp.clone(), securities.clone());
+
+        assert_eq!(lp, commitment.lp);
+        assert_eq!(CommitmentState::PENDING, commitment.state);
+        assert_eq!(securities, commitment.commitments);
+    }
+}
