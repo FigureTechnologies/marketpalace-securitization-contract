5c5059082ae297c2987ab68f8cd22be9e354b48f
Matthew Witkowski <mwitkowski@figure.com>
2023-02-15 14:25:15 -0500

    Added tests for state storage module.

--- src/storage/state.rs
+++ src/storage/state.rs
@@ -36,11 +36,14 @@ pub fn set(storage: &mut dyn Storage, state: &State) -> Result<(), ContractError
 #[cfg(test)]
 mod tests {
     use cosmwasm_std::Addr;
+    use provwasm_mocks::mock_dependencies;
 
     use crate::{
         core::rules::{InvestmentVehicleRule, SettlementDate},
-        storage::state::State,
+        storage::state::{set, State},
     };
+
+    use super::get;
 
     #[test]
     fn test_new_state() {
@@ -58,5 +61,31 @@ mod tests {
         assert_eq!(expected_addr, state.gp);
         assert_eq!(expected_capital_denom, state.capital_denom);
         assert_eq!(expected_rules, state.rules);
+    }
+
+    #[test]
+    fn test_get_invalid() {
+        let deps = mock_dependencies(&[]);
+        get(&deps.storage).unwrap_err();
+    }
+
+    #[test]
+    fn test_get_set_valid() {
+        let mut deps = mock_dependencies(&[]);
+        let expected_addr = Addr::unchecked("address");
+        let expected_capital_denom = "nhash";
+        let expected_rules = vec![InvestmentVehicleRule::SettlementDate {
+            0: SettlementDate {},
+        }];
+        let state = State::new(
+            expected_addr.clone(),
+            expected_capital_denom.to_string(),
+            expected_rules.clone(),
+        );
+
+        set(deps.as_mut().storage, &state).unwrap();
+
+        let obtained = get(&deps.storage).unwrap();
+        assert_eq!(state, obtained);
     }
 }
