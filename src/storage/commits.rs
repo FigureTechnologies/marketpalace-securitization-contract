d23ca7bf2303c0d5b0e9c3a92de2ca03235022de
Matthew Witkowski <mwitkowski@figure.com>
2023-02-15 14:02:14 -0500

    Added tests for commit storage.

--- src/storage/commits.rs
+++ src/storage/commits.rs
@@ -17,4 +17,29 @@ pub fn set(storage: &mut dyn Storage, commitment: &Commitment) -> Result<(), Con
 }
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use cosmwasm_std::Addr;
+    use provwasm_mocks::mock_dependencies;
+
+    use crate::{
+        execute::settlement::commitment::Commitment, storage::commits::get, storage::commits::set,
+    };
+
+    #[test]
+    fn test_get_invalid() {
+        let deps = mock_dependencies(&[]);
+        let lp = Addr::unchecked("bad address");
+        get(&deps.storage, lp).unwrap_err();
+    }
+
+    #[test]
+    fn test_get_set_valid() {
+        let mut deps = mock_dependencies(&[]);
+        let lp = Addr::unchecked("lp");
+        let commitment = Commitment::new(lp.clone(), vec![]);
+        set(deps.as_mut().storage, &commitment).unwrap();
+
+        let obtained = get(deps.as_mut().storage, lp).unwrap();
+        assert_eq!(commitment, obtained);
+    }
+}
