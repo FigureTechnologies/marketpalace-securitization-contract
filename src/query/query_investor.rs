6deb6bf9fe9b10e7ce61da382e0a6a50106c49b8
Matthew Witkowski <mwitkowski@figure.com>
2023-02-21 14:29:25 -0500

    Added tests for query investor.

--- src/query/query_investor.rs
+++ src/query/query_investor.rs
@@ -14,3 +14,39 @@ pub fn query_investor(storage: &dyn Storage, lp: Addr) -> ProvQueryResponse {
     };
     Ok(to_binary(&response)?)
 }
+
+#[cfg(test)]
+mod tests {
+    use cosmwasm_std::{from_binary, testing::mock_env, Addr};
+    use provwasm_mocks::mock_dependencies;
+
+    use crate::{
+        contract::query,
+        core::msg::{QueryInvestorResponse, QueryMsg},
+        execute::settlement::commitment::CommitmentState,
+        util::testing::{
+            create_testing_commitments, instantiate_contract, test_security_commitments,
+        },
+    };
+
+    #[test]
+    fn test_query_investor() {
+        let mut deps = mock_dependencies(&[]);
+        instantiate_contract(deps.as_mut()).expect("should be able to instantiate contract");
+        create_testing_commitments(&mut deps);
+        let res = query(
+            deps.as_ref(),
+            mock_env(),
+            QueryMsg::QueryInvestor {
+                investor: Addr::unchecked("lp1"),
+            },
+        )
+        .unwrap();
+        let value: QueryInvestorResponse = from_binary(&res).unwrap();
+        assert_eq!(test_security_commitments(), value.paid_in_capital);
+        assert_eq!(Addr::unchecked("lp1"), value.commitment.lp);
+        assert_eq!(test_security_commitments(), value.commitment.commitments);
+        // They are not settled until it is withdrawn
+        assert_eq!(CommitmentState::ACCEPTED, value.commitment.state);
+    }
+}
--- src/util/testing.rs
+++ src/util/testing.rs
@@ -148,6 +148,28 @@ pub fn accept_test_commitment(
     execute(deps, env, info, msg)
 }
 
+pub fn test_deposit_message(deposit: &[SecurityCommitment]) -> ExecuteMsg {
+    ExecuteMsg::DepositCommitment {
+        securities: deposit.to_vec(),
+    }
+}
+
+pub fn deposit_test(
+    deps: ProvDepsMut,
+    env: Env,
+    sender: &str,
+    deposit: &[SecurityCommitment],
+) -> ProvTxResponse {
+    let funds = deposit
+        .iter()
+        .fold(Coin::new(0, "denom".to_string()), |acc, commit| -> Coin {
+            Coin::new((commit.amount * 100) + acc.amount.u128(), acc.denom)
+        });
+    let info = mock_info(sender, &vec![funds]);
+    let msg = test_deposit_message(deposit);
+    execute(deps, env, info, msg)
+}
+
 pub type MockDeps = OwnedDeps<MockStorage, MockApi, ProvenanceMockQuerier, ProvenanceQuery>;
 
 pub fn create_testing_commitments(deps: &mut MockDeps) {
@@ -163,11 +185,20 @@ pub fn create_testing_commitments(deps: &mut MockDeps) {
     accept_test_commitment(deps.as_mut(), mock_env(), "gp", &vec!["lp1", "lp2", "lp3"])
         .expect("should be able to accept defined lps");
 
-    // Have a deposit for 1,2
+    // Deposit 1 completely, and partial 2
+    deposit_test(
+        deps.as_mut(),
+        mock_env(),
+        "lp1",
+        &test_security_commitments(),
+    )
+    .expect("should be able to deposit full amount");
 
-    // We have 3 pending
-    // We have 2 accepted
-    // We have 1 settled
+    let mut partial_commitments = test_security_commitments();
+    for partial_commitment in &mut partial_commitments {
+        partial_commitment.amount = partial_commitment.amount / 2;
+    }
 
-    // We need to accept
+    deposit_test(deps.as_mut(), mock_env(), "lp2", &partial_commitments)
+        .expect("should be able to deposit partial amount");
 }
