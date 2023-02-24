cce0429d79f4e42f2dac2c9d614d2df032e7db30
Matthew Witkowski <mwitkowski@figure.com>
2023-02-24 10:04:16 -0500

    Fixed validate and added tests for query responses.

--- crates/contract/src/query/mod.rs
+++ crates/contract/src/query/mod.rs
@@ -1,11 +1,8 @@
-use cosmwasm_std::{Coin, Env};
+use cosmwasm_std::Env;
 
-use crate::{
-    core::{
-        aliases::{ProvDeps, ProvQueryResponse},
-        msg::QueryMsg,
-    },
-    util::validate::{Validate, ValidateResult},
+use crate::core::{
+    aliases::{ProvDeps, ProvQueryResponse},
+    msg::QueryMsg,
 };
 
 mod query_investor;
@@ -13,6 +10,7 @@ mod query_pending_commitments;
 mod query_securitizations;
 mod query_state;
 mod query_version;
+mod validate;
 
 pub fn handle(deps: ProvDeps, _env: Env, msg: QueryMsg) -> ProvQueryResponse {
     match msg {
@@ -30,12 +28,71 @@ pub fn handle(deps: ProvDeps, _env: Env, msg: QueryMsg) -> ProvQueryResponse {
     }
 }
 
-impl Validate for QueryMsg {
-    fn validate(&self) -> ValidateResult {
-        Ok(())
+#[cfg(test)]
+mod tests {
+    use cosmwasm_std::{from_binary, testing::mock_env, Addr};
+    use provwasm_mocks::mock_dependencies;
+
+    use crate::{
+        core::msg::{
+            QueryInvestorResponse, QueryPendingCommitmentsResponse, QuerySecuritizationsResponse,
+            QueryStateResponse, QueryVersionResponse,
+        },
+        util,
+    };
+
+    use super::handle;
+
+    #[test]
+    fn tests_query_investor_has_correct_response() {
+        let mut deps = mock_dependencies(&[]);
+        let msg = crate::core::msg::QueryMsg::QueryInvestor {
+            investor: Addr::unchecked("lp1"),
+        };
+        util::testing::instantiate_contract(deps.as_mut()).unwrap();
+        util::testing::propose_test_commitment(deps.as_mut(), mock_env(), "lp1").unwrap();
+        let bin = handle(deps.as_ref(), mock_env(), msg).unwrap();
+        let _: QueryInvestorResponse = from_binary(&bin).unwrap();
     }
 
-    fn validate_msg_funds(&self, _funds: &[Coin]) -> ValidateResult {
-        Ok(())
+    #[test]
+    fn tests_query_pending_commits_has_correct_response() {
+        let mut deps = mock_dependencies(&[]);
+        let msg = crate::core::msg::QueryMsg::QueryPendingCommitments {};
+        util::testing::instantiate_contract(deps.as_mut()).unwrap();
+        util::testing::propose_test_commitment(deps.as_mut(), mock_env(), "lp1").unwrap();
+        let bin = handle(deps.as_ref(), mock_env(), msg).unwrap();
+        let _: QueryPendingCommitmentsResponse = from_binary(&bin).unwrap();
+    }
+
+    #[test]
+    fn tests_query_state_has_correct_response() {
+        let mut deps = mock_dependencies(&[]);
+        let msg = crate::core::msg::QueryMsg::QueryState {};
+        util::testing::instantiate_contract(deps.as_mut()).unwrap();
+        util::testing::propose_test_commitment(deps.as_mut(), mock_env(), "lp1").unwrap();
+        let bin = handle(deps.as_ref(), mock_env(), msg).unwrap();
+        let _: QueryStateResponse = from_binary(&bin).unwrap();
+    }
+
+    #[test]
+    fn tests_query_version_has_correct_response() {
+        let mut deps = mock_dependencies(&[]);
+        let msg = crate::core::msg::QueryMsg::QueryVersion {};
+        util::testing::instantiate_contract(deps.as_mut()).unwrap();
+        util::testing::propose_test_commitment(deps.as_mut(), mock_env(), "lp1").unwrap();
+        let bin = handle(deps.as_ref(), mock_env(), msg).unwrap();
+        let _: QueryVersionResponse = from_binary(&bin).unwrap();
+    }
+
+    #[test]
+    fn tests_query_securitizations_has_correct_response() {
+        let mut deps = mock_dependencies(&[]);
+        let msg = crate::core::msg::QueryMsg::QuerySecuritizations {
+            securities: vec!["Security1".to_string()],
+        };
+        util::testing::instantiate_contract(deps.as_mut()).unwrap();
+        let bin = handle(deps.as_ref(), mock_env(), msg).unwrap();
+        let _: QuerySecuritizationsResponse = from_binary(&bin).unwrap();
     }
 }
--- crates/contract/src/query/validate.rs
+++ crates/contract/src/query/validate.rs
@@ -1,3 +1,10 @@
+use cosmwasm_std::Coin;
+
+use crate::{
+    core::msg::QueryMsg,
+    util::validate::{Validate, ValidateResult},
+};
+
 impl Validate for QueryMsg {
     fn validate(&self) -> ValidateResult {
         Ok(())
