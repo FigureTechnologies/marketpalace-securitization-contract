c5f323eb2701eec7ed6d361bbef42bb0f32bd85a
Matthew Witkowski <mwitkowski@figure.com>
2023-03-08 13:21:47 -0500

    Added tests for contract storage.

--- crates/contract-controller/src/storage/contract.rs
+++ crates/contract-controller/src/storage/contract.rs
@@ -26,7 +26,7 @@ pub fn list(storage: &dyn Storage) -> Vec<Addr> {
     contracts
 }
 
-pub fn range(storage: &mut dyn Storage, start: Option<&Addr>, amount: u128) -> Vec<Addr> {
+pub fn range(storage: &dyn Storage, start: Option<&Addr>, amount: u128) -> Vec<Addr> {
     let min = start.map(Bound::exclusive);
     let contracts: Vec<Addr> = match amount {
         0 => CONTRACTS_MAP
@@ -40,4 +40,140 @@ pub fn range(storage: &mut dyn Storage, start: Option<&Addr>, amount: u128) -> V
             .collect(),
     };
     contracts
+}
+
+#[cfg(test)]
+mod tests {
+    use cosmwasm_std::Addr;
+    use provwasm_mocks::mock_dependencies;
+
+    use crate::storage::contract::{add, range, remove};
+
+    use super::{has, list, CONTRACTS_MAP};
+
+    #[test]
+    fn test_has_success() {
+        let mut deps = mock_dependencies(&[]);
+        let contract = Addr::unchecked("addr");
+        CONTRACTS_MAP
+            .save(deps.as_mut().storage, &contract, &true)
+            .unwrap();
+        assert_eq!(true, has(&deps.storage, &contract));
+    }
+
+    #[test]
+    fn test_has_failure() {
+        let deps = mock_dependencies(&[]);
+        let contract = Addr::unchecked("addr");
+        assert_eq!(false, has(&deps.storage, &contract));
+    }
+
+    #[test]
+    fn test_add() {
+        let mut deps = mock_dependencies(&[]);
+        let contract = Addr::unchecked("addr");
+        let contract2 = Addr::unchecked("addr2");
+        add(deps.as_mut().storage, &contract).unwrap();
+        add(deps.as_mut().storage, &contract2).unwrap();
+        assert_eq!(true, has(&deps.storage, &contract));
+        assert_eq!(true, has(&deps.storage, &contract2));
+    }
+
+    #[test]
+    fn test_remove() {
+        let mut deps = mock_dependencies(&[]);
+        let contract = Addr::unchecked("addr");
+        let contract2 = Addr::unchecked("addr2");
+        add(deps.as_mut().storage, &contract).unwrap();
+        add(deps.as_mut().storage, &contract2).unwrap();
+        remove(deps.as_mut().storage, &contract);
+        assert_eq!(false, has(&deps.storage, &contract));
+        assert_eq!(true, has(&deps.storage, &contract2));
+    }
+
+    #[test]
+    fn test_remove_non_existant() {
+        let mut deps = mock_dependencies(&[]);
+        let contract = Addr::unchecked("addr");
+        remove(deps.as_mut().storage, &contract);
+        assert!(true);
+    }
+
+    #[test]
+    fn test_list_non_empty() {
+        let mut deps = mock_dependencies(&[]);
+        let contract = Addr::unchecked("addr");
+        let contract2 = Addr::unchecked("addr2");
+        add(deps.as_mut().storage, &contract).unwrap();
+        add(deps.as_mut().storage, &contract2).unwrap();
+        let addresses = list(&deps.storage);
+
+        assert_eq!(vec![contract, contract2], addresses);
+    }
+
+    #[test]
+    fn test_list_empty() {
+        let deps = mock_dependencies(&[]);
+        let addresses = list(&deps.storage);
+        let expected: Vec<Addr> = vec![];
+        assert_eq!(expected, addresses);
+    }
+
+    #[test]
+    fn test_range_starts_at_beginning() {
+        let mut deps = mock_dependencies(&[]);
+        let contract = Addr::unchecked("addr");
+        let contract2 = Addr::unchecked("addr2");
+        add(deps.as_mut().storage, &contract).unwrap();
+        add(deps.as_mut().storage, &contract2).unwrap();
+        let addresses = range(&deps.storage, None, 1);
+
+        assert_eq!(vec![contract], addresses);
+    }
+
+    #[test]
+    fn test_range_starts_at_location() {
+        let mut deps = mock_dependencies(&[]);
+        let contract = Addr::unchecked("addr");
+        let contract2 = Addr::unchecked("addr2");
+        add(deps.as_mut().storage, &contract).unwrap();
+        add(deps.as_mut().storage, &contract2).unwrap();
+        let addresses = range(&deps.storage, None, 1);
+        let addresses = range(&deps.storage, Some(&addresses[0]), 1);
+
+        assert_eq!(vec![contract2], addresses);
+    }
+
+    #[test]
+    fn test_range_handles_zero_elements() {
+        let deps = mock_dependencies(&[]);
+        let addresses = range(&deps.storage, None, 1);
+        let expected: Vec<Addr> = vec![];
+
+        assert_eq!(expected, addresses);
+    }
+
+    #[test]
+    fn test_range_returns_all_for_zero_length() {
+        let mut deps = mock_dependencies(&[]);
+        let contract = Addr::unchecked("addr");
+        let contract2 = Addr::unchecked("addr2");
+        add(deps.as_mut().storage, &contract).unwrap();
+        add(deps.as_mut().storage, &contract2).unwrap();
+        let addresses = range(&deps.storage, None, 0);
+
+        assert_eq!(vec![contract, contract2], addresses);
+    }
+
+    #[test]
+    fn test_range_doesnt_exceed_elements() {
+        let mut deps = mock_dependencies(&[]);
+        let contract = Addr::unchecked("addr");
+        let contract2 = Addr::unchecked("addr2");
+        add(deps.as_mut().storage, &contract).unwrap();
+        add(deps.as_mut().storage, &contract2).unwrap();
+        let addresses = range(&deps.storage, None, 5);
+
+        assert_eq!(vec![contract, contract2], addresses);
+    }
 }
