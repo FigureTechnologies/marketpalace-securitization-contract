617164c009eefebd73d4488425d9c5ffab1a2e14
Matthew Witkowski <mwitkowski@figure.com>
2023-03-10 06:46:20 -0500

    Added tests for migrate all contracts.

--- crates/contract-controller/src/execute/migrate_all_contracts.rs
+++ crates/contract-controller/src/execute/migrate_all_contracts.rs
@@ -35,4 +35,124 @@ pub fn handle(deps: ProvDepsMut, env: Env, sender: Addr, contract_id: Uint128) -
 }
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use cosmwasm_std::{testing::mock_env, Addr, Attribute, Uint128};
+
+    use crate::{
+        core::error::ContractError,
+        execute::{add_contracts, migrate_all_contracts},
+        storage,
+        util::testing::{create_admin_deps, instantiate_contract, migrate_message},
+    };
+
+    #[test]
+    fn test_handle_is_admin() {
+        let mut deps = create_admin_deps(&[]);
+        let env = mock_env();
+        let sender = Addr::unchecked("sender");
+        let contract_id = Uint128::new(2);
+        let res =
+            migrate_all_contracts::handle(deps.as_mut(), env, sender, contract_id).unwrap_err();
+        assert_eq!(ContractError::Unauthorized {}.to_string(), res.to_string());
+    }
+
+    #[test]
+    fn test_handle_works_with_no_managed_contracts() {
+        let mut deps = create_admin_deps(&[]);
+        let env = mock_env();
+        let sender = Addr::unchecked("admin");
+        let contract_id = Uint128::new(2);
+
+        instantiate_contract(deps.as_mut(), env.clone()).unwrap();
+        let res = migrate_all_contracts::handle(deps.as_mut(), env, sender, contract_id).unwrap();
+        let state = storage::state::get(&deps.storage).unwrap();
+
+        assert_eq!(false, state.migrating);
+        assert_eq!(
+            vec![
+                Attribute::new("migration_finished", "true"),
+                Attribute::new("action", "migrate_all_contracts")
+            ],
+            res.attributes
+        );
+        assert_eq!(0, res.events.len());
+        assert_eq!(0, res.messages.len());
+    }
+
+    #[test]
+    fn test_handle_migrates_in_batches() {
+        let mut deps = create_admin_deps(&[]);
+        let env = mock_env();
+        let sender = Addr::unchecked("admin");
+        let contract_id = Uint128::new(2);
+        let contracts = vec![
+            Addr::unchecked("contract1"),
+            Addr::unchecked("contract2"),
+            Addr::unchecked("contract3"),
+        ];
+
+        instantiate_contract(deps.as_mut(), env.clone()).unwrap();
+
+        add_contracts::handle(
+            deps.as_mut(),
+            env.clone(),
+            sender.clone(),
+            contracts.clone(),
+        )
+        .unwrap();
+
+        let res =
+            migrate_all_contracts::handle(deps.as_mut(), env.clone(), sender.clone(), contract_id)
+                .unwrap();
+        let state = storage::state::get(&deps.storage).unwrap();
+        assert_eq!(true, state.migrating);
+        assert_eq!(
+            vec![
+                Attribute::new("migration_finished", "false"),
+                Attribute::new("action", "migrate_all_contracts")
+            ],
+            res.attributes
+        );
+        assert_eq!(0, res.events.len());
+        assert_eq!(
+            vec![
+                migrate_message(contracts[0].clone(), Uint128::new(2), 0),
+                migrate_message(contracts[1].clone(), Uint128::new(2), 1)
+            ],
+            res.messages
+        );
+
+        let res =
+            migrate_all_contracts::handle(deps.as_mut(), env.clone(), sender.clone(), contract_id)
+                .unwrap();
+        let state = storage::state::get(&deps.storage).unwrap();
+        assert_eq!(true, state.migrating);
+        assert_eq!(
+            vec![
+                Attribute::new("migration_finished", "false"),
+                Attribute::new("action", "migrate_all_contracts")
+            ],
+            res.attributes
+        );
+        assert_eq!(0, res.events.len());
+        assert_eq!(
+            vec![migrate_message(contracts[2].clone(), Uint128::new(2), 2)],
+            res.messages
+        );
+
+        let res =
+            migrate_all_contracts::handle(deps.as_mut(), env.clone(), sender.clone(), contract_id)
+                .unwrap();
+        let state = storage::state::get(&deps.storage).unwrap();
+        assert_eq!(false, state.migrating);
+        assert_eq!(
+            vec![
+                Attribute::new("migration_finished", "true"),
+                Attribute::new("action", "migrate_all_contracts")
+            ],
+            res.attributes
+        );
+        assert_eq!(0, res.events.len());
+        assert_eq!(0, res.messages.len());
+    }
+}
