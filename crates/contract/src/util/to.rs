4687946380d362e4afb56a4fa2a1527c70d0bfd2
Matthew Witkowski <mwitkowski@figure.com>
2023-03-22 13:12:11 -0400

    Finished documentation for util.

--- crates/contract/src/util/to.rs
+++ crates/contract/src/util/to.rs
@@ -1,7 +1,24 @@
 use cosmwasm_std::Addr;
 
-pub fn security_to_investment_name(name: &String, contract: &Addr) -> String {
-    format! {"{}.{}", contract, name }
+/// Creates an investment token name for the supplied security name.
+///
+/// # Parameters
+///
+/// * `security_name` A reference to the name of the security.
+/// * `contract` A reference to the address of the contract
+///
+/// # Examples
+/// ```
+/// use contract::util::to::security_to_investment_name;
+///
+/// let contract_address = Addr::unchecked("contract_address");
+/// let security_name = "MySecurity".to_string();
+/// let investment_name = security_to_investment_name(&security_name, &contract_address);
+/// assert_eq!("contract_address.MySecurity".to_string(), investment_name);
+///
+/// ```
+pub fn security_to_investment_name(security_name: &String, contract: &Addr) -> String {
+    format! {"{}.{}", contract, security_name }
 }
 
 #[cfg(test)]
