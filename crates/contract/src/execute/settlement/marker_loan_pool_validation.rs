60e4a587b09147fc7252e582fb0538487fbf8345
arnabmitra <arnabmitra.mitra@gmail.com>
2023-08-15 18:00:23 -0400

    Adding documentation.

--- crates/contract/src/execute/settlement/marker_loan_pool_validation.rs
+++ crates/contract/src/execute/settlement/marker_loan_pool_validation.rs
@@ -15,7 +15,23 @@ fn get_contract_error(msg: String) -> Result<(), ContractError> {
         .to_err()
 }
 
-
+/// Validates the marker for loan pool addition or removal.
+///
+/// This function checks whether the passed marker meets the certain criteria to be added to a loan pool.
+/// It ensures that all the necessary permissions on the marker are granted.
+/// It checks if the marker is Active and whether it holds an amount of its own denomination (`denom`).
+/// The function also checks whether the bank supply is less than what is held in the marker.
+///
+/// # Arguments
+/// * `marker` - A reference to the `Marker` to be validated.
+/// * `original_owner_address` - An optional address. If provided, the function checks whether this address has administrative privileges on the marker.
+/// * `contract_address` - The address of the contract.
+/// * `expected_contract_permissions` - A reference to the permissions that the contract is expected to have on the marker.
+/// * `bank_supply` - The total supply held by the bank.
+///
+/// # Returns
+/// * Returns a `Result` with `Ok(())` if the `marker` has the `expected_contract_permissions`, is Active, holds some of its `denom`, and doesn't hold more than what is supplied by the bank.
+/// * Returns a `Result` with `Err(ContractError)` if any of the checks fails. Each failure case has a unique error message that describes what went wrong.
 pub fn validate_marker_for_loan_pool_add_remove(
     marker: &Marker,
     original_owner_address: Option<&Addr>,
@@ -47,7 +63,7 @@ pub fn validate_marker_for_loan_pool_add_remove(
             marker.denom, marker.status,
         ));
     }
-    // get denom that this marker holds
+    // get denom that this marker holds, where denom == marker denom
     let marker_coin = get_single_marker_coin_holding(marker)?;
 
     if marker_coin.amount.u128() == 0 {
