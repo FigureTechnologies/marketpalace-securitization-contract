3ef2bec3f8782ee4a629cf4d94c289be4b1774f9
arnabmitra <arnabmitra.mitra@gmail.com>
2023-08-15 16:38:55 -0400

    Refactor some code.

--- crates/contract/src/execute/settlement/marker_loan_pool_validation.rs
+++ crates/contract/src/execute/settlement/marker_loan_pool_validation.rs
@@ -6,6 +6,16 @@ use crate::util::provenance_utilities::{
 use cosmwasm_std::{Addr, Uint128};
 use provwasm_std::{Marker, MarkerAccess, MarkerStatus};
 
+
+// New helper function for generating error messages
+fn get_contract_error(msg: String) -> Result<(), ContractError> {
+    ContractError::InvalidMarker {
+        message: msg,
+    }
+        .to_err()
+}
+
+
 pub fn validate_marker_for_loan_pool_add_remove(
     marker: &Marker,
     original_owner_address: Option<&Addr>,
@@ -15,73 +25,56 @@ pub fn validate_marker_for_loan_pool_add_remove(
 ) -> Result<(), ContractError> {
     if let Some(original_owner_address) = original_owner_address {
         if !marker_has_admin(marker, original_owner_address) {
-            return ContractError::InvalidMarker {
-                message: format!(
-                    "expected sender [{}] to have admin privileges on marker [{}]",
-                    original_owner_address.as_str(),
-                    marker.denom,
-                ),
-            }
-            .to_err();
+            return get_contract_error(format!(
+                "expected sender [{}] to have admin privileges on marker [{}]",
+                original_owner_address.as_str(),
+                marker.denom,
+            ));
         }
     }
     if !marker_has_permissions(marker, contract_address, expected_contract_permissions) {
-        return ContractError::InvalidMarker {
-            message: format!(
-                "expected this contract [{}] to have privileges {:?} on marker [{}]",
-                contract_address.as_str(),
-                expected_contract_permissions,
-                marker.denom,
-            ),
-        }
-        .to_err();
+        return get_contract_error(format!(
+            "expected this contract [{}] to have privileges {:?} on marker [{}]",
+            contract_address.as_str(),
+            expected_contract_permissions,
+            marker.denom,
+        ));
     }
     // Active check
     if marker.status != MarkerStatus::Active {
-        return ContractError::InvalidMarker {
-            message: format!(
-                "expected marker [{}] to be active, but was in status [{:?}]",
-                marker.denom, marker.status,
-            ),
-        }
-        .to_err();
+        return get_contract_error(format!(
+            "expected marker [{}] to be active, but was in status [{:?}]",
+            marker.denom, marker.status,
+        ));
     }
     // get denom that this marker holds
     let marker_coin = get_single_marker_coin_holding(marker)?;
 
     if marker_coin.amount.u128() == 0 {
-        return ContractError::InvalidMarker {
-            message: format!(
+        return get_contract_error(
+            format!(
                 "expected marker [{}] to hold at least one of its supply of denom, but it had [{}]",
                 marker.denom,
                 marker_coin.amount.u128(),
-            ),
-        }
-        .to_err();
+            ));
     }
     // supply fixed then we can trust the marker total_supply
     if marker.supply_fixed {
-        if marker_coin.amount < marker.total_supply.atomics() {
-            return ContractError::InvalidMarker {
-                message: format!(
-                    "expected marker [{}] to be holding all the shares with supply [{}]",
-                    marker.denom,
-                    marker_coin.amount.u128(),
-                ),
-            }
-            .to_err();
+        if marker_coin.amount > marker.total_supply.atomics() {
+            return get_contract_error(format!(
+                "expected marker [{}] to be holding all the shares with supply [{}]",
+                marker.denom,
+                marker_coin.amount.u128(),
+            ));
         }
     } else {
         // use the bank supply passed in
         if marker_coin.amount > bank_supply {
-            return ContractError::InvalidMarker {
-                message: format!(
-                    "expected that marker, [{}] to be holding all the shares with supply, [{}]",
-                    marker.denom,
-                    marker_coin.amount.u128(),
-                ),
-            }
-            .to_err();
+            return get_contract_error(format!(
+                "expected that marker, [{}] to be holding all the shares with supply, [{}]",
+                marker.denom,
+                marker_coin.amount.u128(),
+            ));
         }
     }
 
