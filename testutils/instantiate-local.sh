1ceacf6a607c6f572921c1aa8de73def2ca5ed4d
arnabmitra <arnabmitra.mitra@gmail.com>
2023-08-16 14:24:42 -0400

    small twek to test deploy script.

--- testutils/instantiate-local.sh
+++ testutils/instantiate-local.sh
@@ -12,4 +12,4 @@ else
 fi
 
 INIT='{"gp":"tp13k86awgexqdt2f2wtu6ukdhrg8dc8nrtmc49pl","securities":[{"name":"Security1","amount":"1000","security_type":{"tranche":{}},"minimum_amount":"10","price_per_unit":{"denom":"nhash","amount":"1000000000"}}],"capital_denom":"nhash"}'
-${PROVENANCE_DIR}/build/provenanced -t tx wasm instantiate "$CODE_ID" "$INIT" --label "something" --from validator --home ${PROVENANCE_DIR}/build/run/provenanced  --node http://localhost:26657 --chain-id testing --gas-prices 190500nhash --gas auto --gas-adjustment 2 --output json -b block --no-admin -y | jq
+${PROVENANCE_DIR}/build/provenanced -t tx wasm instantiate "$CODE_ID" "$INIT" --label "securitization.pb" --from validator --home ${PROVENANCE_DIR}/build/run/provenanced  --node http://localhost:26657 --chain-id testing --gas-prices 190500nhash --gas auto --gas-adjustment 2 --output json -b block --no-admin -y | jq
